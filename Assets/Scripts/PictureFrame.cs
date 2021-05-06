a599dfb7dd4eb0e4482f1ff882de20c2ed30e75a
Soraya McGarrh <shm200000@utdallas.edu>
2021-05-06 14:33:13 -0500

    Added Mario and beginning walk

--- Assets/Scripts/PictureFrame.cs
+++ Assets/Scripts/PictureFrame.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+[RequireComponent(typeof(AudioSource))]
 public class PictureFrame : MonoBehaviour
 {
 
 public GameObject player;
-
+public AudioSource source;
+    
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+    
 
  void OnTriggerEnter(Collider other)
  {
      SceneManager.LoadScene(1);
      player.SetActive(false);
+     
  }
+
 
 }
