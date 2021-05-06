1383de436d5eaaee97f224ba180b120985d455c4
Soraya McGarrh <shm200000@utdallas.edu>
2021-05-05 21:15:26 -0500

    Scene Manager

---
+++ Assets/Scripts/ManagerScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerScene : MonoBehaviour
+{
+void Start()
+    {
+        
+        Cursor.visible = false;
+    }
+
+    void Update()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            
+
+        }
+
+}
---
+++ Assets/Scripts/PictureFrame.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PictureFrame : MonoBehaviour
+{
+
+public GameObject player;
+
+
+ void OnTriggerEnter(Collider other)
+ {
+     SceneManager.LoadScene(1);
+     player.SetActive(false);
+ }
+
+}
